6ece2fe057875f6555c9e264be0dceba742a0acf
sudarshan sangle <darshsangle9456@gmail.com>
2025-10-15 22:10:08 +0530

    DONE

--- frontend/server.js
+++ frontend/server.js
@@ -7,10 +7,11 @@ const app = express();
 
 // Set up view engine and middleware
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "frontend/views"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "frontend/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 const QUIZ_SERVICE_URL = "http://localhost:5001";
 const RESULT_SERVICE_URL = "http://localhost:5002";
